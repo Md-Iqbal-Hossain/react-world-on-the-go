776b3cc4e6618426e8f78550ce0beed12fe46eb8
Iqbal <iqbalhossain6615@gmail.com>
2026-03-25 00:21:32 +0600

    button and its functionalities added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css';
+import './Country.css'
 
 const Country = ({ country }) => {
+    const [visited, setVisited] = useState(false)
     // console.log(country);
+
+    const handleVisited = () => {
+        // basic system 
+        // if(visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        // second system 
+        // setVisited(visited ? false : true)
+
+        // 3rd system 
+        setVisited(!visited)
+        
+    }
     
     return (
-        <div>
+        // <div className={`country ${visited ? 'country-visited' : 'country-not-visited'}`}>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000   ? 'Big Country' : 'Small Country'}</p>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
